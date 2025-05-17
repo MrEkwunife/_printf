1034485592d43c9d0568b9bd95c6de5ff4eedeef
Emmanuel <kcemmy03@gmail.com>
2025-05-17 17:26:04 +0100

    Function to write a character to stdout

--- putchar.c
+++ putchar.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * _putchar - Writes a 1 byte character to stdout
+ *
+ * @c: the character to be written
+ * Return: The character written
+*/
 int _putchar(char c)
 {
     return (write(1, &c, 1));
