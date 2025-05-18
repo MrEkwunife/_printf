e68dcd57b6010e18b98656470385b9ff575aff8a
Emmanuel <kcemmy03@gmail.com>
2025-05-18 20:10:19 +0100

    Implemented function to get each digit in a number

---
+++ basic_utils.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * get_integer_digit - This function prints each digits in a number to stdout.
+ *
+ * @number: The number to be worked on.
+ * Return: The number of digits in the number.
+ */
+int get_integer_digits(int number)
+{
+    int div, temp, inc = 1, count = 0;
+
+    if (number < 0)
+        return (0);
+    
+    temp = number;
+    while (temp > 10)
+    {
+        temp /= 10;
+        inc *= 10;
+    }
+    while (inc > 0)
+    {
+        div = number / inc;
+        number %= inc;
+        _putchar(div + '0');
+        count += 1;
+
+        inc /= 10;
+    }
+    
+    return (count);
+}
