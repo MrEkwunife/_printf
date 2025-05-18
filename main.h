9e1a142646cdc55f0a62415827aac601c43323c1
Emmanuel <kcemmy03@gmail.com>
2025-05-18 20:10:51 +0100

    Add function prototype to header file

--- main.h
+++ main.h
@@ -10,6 +10,8 @@ int _printf(const char *format, ...);
 int _strlen(const char *format);
 int print_string(va_list args);
 int print_character(va_list args);
+int print_integer(va_list args);
 int handle_format_specifier(char c, va_list args);
+int get_integer_digits(int number);
 
 #endif
