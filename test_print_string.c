77242d491407f74f64321a5dcf94642b93fd860b
Emmanuel <kcemmy03@gmail.com>
2025-05-18 20:14:44 +0100

    Test: add test for integer format specifier

--- test_print_string.c
+++ test_print_string.c
@@ -3,28 +3,33 @@
 
 int main(void)
 {
-    // Test 1
-    int n = _printf("Name:%s%c\n", "CHIMOBI", 'c');
-    printf("%d\n", n);
+    int n, o, i;
+
+    /* Test 1 */
+    n = _printf("Name:%s%c\n", "CHIMOBI", 'c');
+    _printf("%d\n", n);
     /**
      * Name:CHIMOBIc
      * 14
      */
 
-    // Test 2
-    int o = _printf("My name is %s\n", "IFEDI");
-    printf("%d\n", o);
+    /* Test 2 */
+    o = _printf("My name is %s\n", "IFEDI");
+    _printf("%d\n", o);
     /**
      * My name is  IFEDI
      * 17
      */
 
-    // Test 3
-    int i = _printf("Oh %c%c\n", 'h', 'i');
-    printf("%d\n", i);
+    /* Test 3 */
+    i = _printf("Oh %c%c\n", 'h', 'i');
+    _printf("%d\n", i);
     /**
      * Oh hi
      * 6
      */
+    /* Test integers*/
+    _printf("%d\n", 123456789);
+    _printf("%i\n", 987654321);
     return (0);
 }
