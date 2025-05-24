dfc7f4a6b115deb567999a8ffc08d4269b4d6c72
Emmanuel <kcemmy03@gmail.com>
2025-05-24 14:47:47 +0100

    Modified integer functions to parse negative numbers

--- basic_utils.c
+++ basic_utils.c
@@ -1,6 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 
+#define ABS(a) ((a) < 0 ? -(a) : (a))
+
 /**
  * get_integer_digit - This function prints each digits in a number to stdout.
  *
@@ -11,17 +13,20 @@ int print_integer_digits(int number)
 {
     int div, temp, inc = 1, count = 0;
 
-    if (number < 0)
-        return (0);
+    if (number < 0){
+        _putchar('-');
+        count += 1;
+    }
 
-    temp = number;
+    temp = ABS(number);
     temp /= 10;
     while (temp > 0)
     {
         temp /= 10;
         inc *= 10;
     }
-
+    
+    number = ABS(number);
     while (inc > 0)
     {
         div = number / inc;
